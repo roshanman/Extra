a0c6632fe7d3dd03b48f7c4da932378c57e4fbb3
ebt <ebt@ebt-no-MacBook-Pro.local>
2017-11-03 10:20:47 +0800

    init

--- Example/Extra/ViewController.swift
+++ Example/Extra/ViewController.swift
@@ -7,18 +7,73 @@
 //
 
 import UIKit
+import Extra
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dest = segue.destination
+        
+        dest.extra.put(key: "title", value: "ViewController2")
+        dest.extra.put(key: "textView", value: "Created by morenotepad@163.com")
+        dest.extra.put(key: "button", value: "BackButton")
+        dest.extra.put(key: "imageView", value: #imageLiteral(resourceName: "button.png"))
+        dest.extra.put(key: "progressView", value: Float(0.8))
+        dest.extra.put(key: "switch", value: false)
+        dest.extra.put(key: "backgroundColor", value: UIColor.white)
+        dest.extra.put(key: "progressTintColor", value: UIColor.red)
+        
+        let block: (Void) -> Void = {
+            let alert = UIAlertController(
+                title: "Test Block",
+                message: nil, 
+                preferredStyle: .alert
+            )
+            
+            let action = UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: nil
+            )
+            
+            alert.addAction(action)
+            
+            self.navigationController?.present(
+                alert,
+                animated: true,
+                completion: nil
+            )
+        }
+        
+        dest.extra.put(key: "block", value: block)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var `switch`: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title                   = extra.get(key: "title")
+        textView.text           = extra.get(key: "textView")
+        imageView.image         = extra.get(key: "imageView")
+        `switch`.isOn           = extra.get(key: "switch") ?? true
+        view.backgroundColor    = extra.get(key: "backgroundColor")
+        progressView.progress   = extra.get(key: "progressView") ?? 0.1
+        progressView.progressTintColor = extra.get(key: "progressTintColor")
+
+        button.setTitle(extra.get(key: "button"), for: .normal)
+    }
+    
+    @IBAction func testBlock(_ sender: Any) {
+        let block: ((Void) -> Void)? = extra.get(key: "block")
+        
+        block?()
+    }
+}
---
+++ Example/Pods/Target Support Files/Pods-Extra_Example/Pods-Extra_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Extra_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Extra_Example
+@end
